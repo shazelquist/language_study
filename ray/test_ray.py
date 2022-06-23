ac089c844213c7bb7cb36a49a8e10dc60ea49005
shazelquist <shazelquis@student.rccd.edu>
2022-06-22 22:59:41 -0700

    Testing ray functionality
    - Added pseudo_db
    	This provides functionality for basic searches and just
    	generally indexing for tests of ray containers.
    - Populated pseudo_db with syllabary information
    - Basic word parsing for language model
    
    Appears that the same ray layout can be used for higher order data,
    although this may not be desireable for actual implementation.
    
    It is also a decent idea to gather some source information from the
    target language in order to generate a colleciton of non-word characters
    or punctuation. Seperating words and thoughts may be more difficult
    without seperatable characters for tokenization.

--- ray/test_ray.py
+++ ray/test_ray.py
@@ -11,6 +11,89 @@ import re
 from ray import *
 
 
+class pseudo_db:
+    """
+    pseudo_db
+
+    container for proof of concept searches
+    """
+
+    def __init__(self):
+        """
+        __init__()
+
+        establishes container
+        """
+        self.cont = {}
+
+    def __obj_label__(self, obj, label=None):
+        """
+        __obj_label__(obj, label)
+
+        determines if object or label
+        """
+        if type(label) != type("") or not label:
+            label = type(obj)
+        return label
+
+    def add(self, obj, label=None):
+        """
+        add(obj, label='')
+
+        adds object to collection
+        """
+        label = self.__obj_label__(obj, label)
+
+        if label not in self.cont.keys():
+            self.cont[label] = []
+        self.cont[label].append(obj)
+
+    def add_all(self, obj, label=None):
+        """
+        add_all(obj)
+
+        adds all objects in given iterable
+        """
+        label = self.__obj_label__(obj, label)
+
+        for item in obj:
+            self.add(item, label)
+
+    def query(self, label, cond):
+        """
+        query(label, cond)
+
+        returns all sucessful conditions for obj type
+        """
+        label = self.__obj_label__(label, label)
+
+        return [item for item in self.cont[label] if cond(item)]
+
+    def count(self, label, cond):
+        """
+        count(label, cond)
+
+        returns number of sucessful conditions
+        """
+        label = self.__obj_label__(label, label)
+
+        return sum([1 for item in self.cont[label] if cond(item)])
+
+    def first(self, label, cond):
+        """
+        first(label, cond)
+
+        returns first match to condition for obj type
+        """
+        label = self.__obj_label__(label, label)
+        for item in self.cont[label]:
+            if cond(item):
+                return item
+
+
+p_db = pseudo_db()
+
+
 def floor(arg):
     """
     floor(arg)
@@ -81,6 +164,10 @@ def main():
     chars = list(set(text))
     chars.sort()
 
+    p_db.add_all(list(range(0, 10)), "zero to ten")
+    print(p_db.cont)
+    input()
+
     # Insert start & terminating character (not very useful with one sample)
     print("$ in base set: {}".format("$" in chars))
     print("^ in base set: {}".format("^" in chars))
@@ -88,7 +175,12 @@ def main():
     chars.append("^")
 
     syllables = [syllabary(c, text.count(c)) for c in chars]
-    print("\n".join([i.__repr__() for i in syllables]))
+    start_syl = syllables[-1]
+
+    p_db.add_all(syllables)
+    # res = p_db.query(syllables[0],lambda slb : slb.sylb == 'a')
+    print(p_db.query(syllables[0], lambda slb: slb.sylb == "a"))
+    # print("\n".join([i.__repr__() for i in syllables]))
     ptext = "^" + text + "$"
     matrix = [0] * (len(chars) * (len(chars) - 2))
     # printmatrix(chars,matrix)
@@ -98,7 +190,38 @@ def main():
     matrix[getindex("$", text[-1], chars)] += 1
     # probability is count/sum of column
     # matrix=[float('{}{}'.format((i%26==0 or i%27==0)*str(matrix[i]),(i%26!=0 and i%27!=0)*str(matrix[i]/35))) for i in range(0,len(matrix))]
-    printmatrix(chars, matrix)
+    # printmatrix(chars, matrix)
+
+    relations = []
+    colsums = [getcolsum(i, matrix, chars) for i in chars] + [1, 1]
+    for i in range(0, len(matrix)):
+        if matrix[i]:
+            parent = p_db.first(start_syl, lambda x: x.sylb == chars[i % len(chars)])
+            child = p_db.first(
+                start_syl, lambda x: x.sylb == chars[floor(i / len(chars))]
+            )
+            relations.append(
+                syll_relation(
+                    parent.sylb, child.sylb, matrix[i] / colsums[i % len(chars)]
+                )
+            )  # /colsums
+            child.freq /= colsums[i % len(chars)]
+    p_db.add_all(relations)
+    print("number of syllables", p_db.count(start_syl, lambda x: True))
+    print("\n".join([str(i) for i in p_db.query(start_syl, lambda x: True)]))
+    print("number of relations", p_db.count(relations[0], lambda x: True))
+    print("\n".join([str(i) for i in p_db.query(relations[0], lambda x: True)]))
+
+    # now do the same thing for words
+    textwords = re.findall(r"[a-zA-Z]+", text)
+    word_set = set(textwords)
+    words = [syllabary(w, textwords.count(w)) for w in word_set]
+    p_db.add_all(words, "words")
+    p_db.count("words", lambda x: x.sylb.lower() == "the")
+    # print(words)
+
+    # parsing words isn't enough, will need to parse out sentences as well.
+    # Each sentence can be used as it's own "source" for a complete thought
 
 
 if __name__ == "__main__":
