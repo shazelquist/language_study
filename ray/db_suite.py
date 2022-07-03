d067c1d1895b9a91550f1c7ca6bf078526882dbc
shazelquist <shazelquis@student.rccd.edu>
2022-07-02 18:25:25 -0700

    Introduced parameter sharing
    - still requires parameter passing notation => refresh func parameter
      types
    - basic format for loading text into database, though object reference
      may require additional collection first.

--- ray/db_suite.py
+++ ray/db_suite.py
@@ -17,9 +17,20 @@ def erase_db(param=None):
     print("opening session")
     session = Session(engine)
 
+def push_sentence(target, sentence_def param=None):
+    
+    for sentence in ['source_beg_token']+re.find_all(sentence_def, target)['source_end_token']:
+        push_relation(target,param)
 
-def push_words(target, param=None):
-    pass
+def push_relation(target, order=1, param=None):
+    
+    if order>1:
+        push_relation(target,order-1,param)
+    if order==1:# push word instances
+        for word in ['sentence_beg_token']+re.find_all(sentence_def, target)['sentence_end_token']:
+            push_characters(word, param)
+    else:# push relation of given order
+        pass
 
 def push_characters(target, param=None):
     pass
@@ -55,20 +66,43 @@ def guess_next(param=None):
     for child in options:
         print(child.text.text,child.probability())
 
+def echo(*param):
+    print(param)
+
+def load_dictionary(param=None):
+    pass
 
 def main():
-    print('starting suite, param:', argv[1:])
-    actions = [i[1:] for i in argv if (i and i[0]=='-')]
+    cm_param = argv[1:]
+    print('starting suite, param:', cm_param)
+    actions = [i[1:] for i in cm_param if (i and i[0]=='-')]
     print('starting actions',actions)
     print('avaliable actions:',dir(mainself))
-    for aindex in range(0,len(actions)):
-        action = actions[aindex]
-        #index = argv.index('-'+action)
+    while actions:
+        print(actions)
+        action = actions[0]
+        plimit = cm_param.index('-'+action)+1
+        while plimit<len(cm_param) and  cm_param[plimit][0]!='-':
+            plimit+=1
+        print(cm_param[cm_param.index('-'+action)+1:plimit])
+
         if action in dir(mainself):
             print('found function "{}"'.format(action))
-            getattr(mainself,action)()
+            getattr(mainself,action)(*cm_param[cm_param.index('-'+action)+1:plimit])
         else:
             print('could not find function "{}"'.format(action))
+
+        del actions[0]
+        del cm_param[:plimit]
+        
+    for aindex in range(0,len(actions)):
+        action = actions[aindex]
+        pstart = cm_param.index('-'+action)
+        plimit = len(cm_param)
+        if aindex<len(actions):
+            plimit = cm_param.index('-'+actions[aindex+1])
+        print('splice [{}:{}]'.format(pstart,plimit),cm_param[pstart:plimit])
+        #index = cm_param.index('-'+action)
     
 
 if __name__=='__main__':
